61334b460919dd108a5733fb0ee5fb955195f4ea
Hailey <72172315+Hailey-beep@users.noreply.github.com>
2022-08-21 20:32:53 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD2RwmAqTw1NwmQN-Mf9n1Cr8Ix4QS3G-w", 
+    authDomain: "spectagram-f5c7a.firebaseapp.com", 
+    projectId: "spectagram-f5c7a", 
+    storageBucket: "spectagram-f5c7a.appspot.com", 
+    messagingSenderId: "689301726485", 
+    appId: "1:689301726485:web:0f8652bc391a783f28751c" 
+};
